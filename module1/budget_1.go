e5a75e6ab23592d849d486faa8922a52acf80c40
Marcus Osborn <modevops@Marcuss-MacBook-Pro.local>
2020-07-15 18:30:29 -0600

    add strct

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,5 +1,16 @@
 package module1
 
 // Budget stores budget information
+type Budget struct {
+	Max      float32
+	Items    []Item
+
+}
+
 
 // Item stores item information
+type Item struct {
+	Description string
+	Price       float32
+
+}
